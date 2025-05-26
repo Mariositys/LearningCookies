2e8f20141694570f8b6850136e7fa7abe8a7763e
Mariositys <151974224+Mariositys@users.noreply.github.com>
2025-05-26 22:07:41 +1000

    feat: Функции добавления/удаления куки и набросок формы регистрации

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,59 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, age=0) {
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + "; max-age=" + age;
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+function RegistrationForm() {
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  function handleClick() {
+    alert(inputValue);
+  }
+
+  return (
+    <>
+      Придумайте свой UID
+      <input type="" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleClick}>
+        Зарегистрироваться
+      </button>
+    </>
+  )
+}
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <RegistrationForm />
     </>
   )
 }
