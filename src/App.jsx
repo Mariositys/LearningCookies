923ae581bd35e11b2e4be54462388bea01f69b83
Mariositys <151974224+Mariositys@users.noreply.github.com>
2025-05-28 18:34:21 +1000

    feat: Функция регистрации/авторизации

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function getUserData() {
@@ -10,15 +8,14 @@ function getUserData() {
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
-function setUserData(userName, userSurname, age=120) {
+function setUserData(userName, userSurname, age=30) {
 
   let updatedCookie = encodeURIComponent("userData") + "=" + encodeURIComponent(userName + ' ' + userSurname) + "; max-age=" + age;
 
   document.cookie = updatedCookie;
 }
 
-function RegistrationForm() {
-
+function RegistrationForm({updateUserData}) {
   const [Name, setName] = useState('');
   const [Surname, setSurname] = useState('');
 
@@ -31,10 +28,8 @@ function RegistrationForm() {
   };
 
   function handleClick() {
-    setUserData(Name, Surname)
-
-    alert(Name + Surname);
-    alert(getUserData());
+    setUserData(Name, Surname);
+    updateUserData(getUserData())
   }
 
   return (
@@ -51,15 +46,79 @@ function RegistrationForm() {
   )
 }
 
+function AdditionalData() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  let loadData = async () => {
+      console.log('loading data');
+      let promise = await fetch('https://randomuser.me/api/')
+        .then((result) => {
+          result.json()
+            .then(result => setData(result.results[0]));
+            setIsLoading(false);
+        }, 
+        (error) => {
+          setError(error);
+          setIsLoading(false);
+        });
+  };
+
+
+  
+  useEffect(() => {
+    console.log('FIRING')
+    if (!data && !error) {
+      setIsLoading(true);
+      loadData();
+    }
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className='loader'></div>
+    )
+  }
+  else {
+    if (!error && data) {
+      return (
+        <>
+          <div>{data.email}</div>
+          <div>{data.cell}</div>
+        </>
+      )
+    }
+    else {
+      return (
+        <div>Не получилось загрузить. Ошибка {error}</div>
+      )
+    }
+  }
+  
+}
 
 
 function App() {
+  const [uData, setuData] = useState(getUserData())
+  console.log('rerender')
 
-  return (
-    <>
-    <RegistrationForm />
-    </>
-  )
+  if (uData === undefined) {
+    return (
+        <RegistrationForm 
+          updateUserData={setuData}
+        />
+      )
+  }
+  else {
+    return(
+      <div>
+        Добро пожаловать {uData}
+        <AdditionalData />
+      </div>
+    )
+  }
+  
 }
 
 export default App
